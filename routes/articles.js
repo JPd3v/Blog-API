88d5e36a584c3f184a0f8e48273f317dc56c879f
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-10-29 05:25:17 -0300

    added api routes of articles

---
+++ routes/articles.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const articlesController = require("../controllers/articleController");
+const commentController = require("../controllers/commentController");
+
+// articles routes
+router.get("/", articlesController.get_articles);
+router.post("/", articlesController.post_article);
+router.get("/:id", articlesController.get_single_article);
+router.put("/:id", articlesController.put_article);
+router.delete("/:id", articlesController.delete_article);
+
+// comments in articles routes
+router.get("/:id/comments", commentController.get_comments_article);
+router.post("/:id/comments", commentController.post_comment_article);
+router.delete(
+  "/:id/comments/:commentId",
+  commentController.delete_comment_article
+);
+
+module.exports = router;
