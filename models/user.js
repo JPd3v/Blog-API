4638ce5f9cb5d7526709f79bba01c99d803fd7bf
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-10-29 05:21:30 -0300

    added user model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  refresh_token: { type: String, default: "" },
+});
+
+UserSchema.set("toJSON", {
+  transform: function (doc, ret, options) {
+    delete ret.refresh_token;
+    return ret;
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
