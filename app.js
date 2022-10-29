1e6246e0f3d198547157649c06bec01c6341c0d0
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-10-29 05:19:55 -0300

    added passportjs and passport strategies

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+
+require("./strategies/localStrategy");
+require("./strategies/jwtStrategy");
 
 app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// mongoDb config
 const mongoDB = process.env.MONGODB_ATLAS_URL;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error"));
 
+require("./models/comment");
+require("./models/user");
+
 app.get("/", (req, res) => {
   res.json({ text: "hello world" });
 });
+
+const articlesRouter = require("./routes/articles");
+const userRouter = require("./routes/user");
+
+app.use(passport.initialize());
+
+// api routes
+app.use("/articles", articlesRouter);
+app.use("/user", userRouter);
 
 const SERVER_PORT = 3000;
 app.listen(SERVER_PORT, () =>
