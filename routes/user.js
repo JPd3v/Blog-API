0ea809d9a937d0062e6c84d5e19ca64e76f409e5
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-10-31 05:50:03 -0300

    added user api routes

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+router.post("/sign-up", userController.post_user_sign_up);
+router.post("/log-in", userController.post_user_log_in);
+router.post("/refresh-token", userController.get_new_token);
+router.get("/me", userController.get_user_info);
+router.get("/log-out", userController.get_log_out);
+
+module.exports = router;
